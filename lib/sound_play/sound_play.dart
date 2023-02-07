eca92d767a61a5132ed9fe127ac6657dc09453a2
Quy Do Ngoc <quydn720@gmail.com>
2023-02-08 00:23:16 +0700

    update file name

--- lib/sound_play/sound_play.dart
+++ lib/sound_play/sound_play.dart
@@ -1,3 +1,3 @@
 export 'cubit/sound_play_cubit.dart';
-export 'view/Soundplay_home_page.dart';
-export 'view/Soundplay_now_playing_page.dart';
+export 'view/soundplay_home_page.dart';
+export 'view/soundplay_now_playing_page.dart';
