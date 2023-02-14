9101153e4a80a1c3cccd999aeff7d550d45c59ff
Quy Do Ngoc <quydn720@gmail.com>
2023-02-14 10:14:51 +0700

    extract to avatar stacked widget

--- lib/habitica/presentation/widgets/task_card.dart
+++ lib/habitica/presentation/widgets/task_card.dart
@@ -95,34 +95,8 @@ class TaskCard extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Expanded(
-                  // width: (size.width - 24) * 0.3,
-                  child: Stack(
-                    children: const [
-                      CircleAvatar(radius: 18),
-                      Positioned(
-                        left: 25,
-                        child: CircleAvatar(
-                          radius: 18,
-                          backgroundColor: Colors.green,
-                        ),
-                      ),
-                      Positioned(
-                        left: 50,
-                        child: CircleAvatar(
-                          radius: 18,
-                          backgroundColor: Colors.yellow,
-                        ),
-                      ),
-                      Positioned(
-                        left: 75,
-                        child: CircleAvatar(
-                          radius: 18,
-                          backgroundColor: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
+                const Expanded(
+                  child: AvatarStacked(avatars: mockAvatars, avatarRadius: 18),
                 ),
                 const SizedBox(width: 4),
                 Row(
@@ -145,6 +119,59 @@ class TaskCard extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class AvatarStacked extends StatelessWidget {
+  const AvatarStacked({
+    super.key,
+    required this.avatars,
+    this.avatarRadius,
+  });
+
+  static const maxLength = 4;
+  static const cutPortion = 0.75;
+
+  final List<String> avatars;
+  final double? avatarRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    final diameter = (avatarRadius ?? 20) * 2;
+    final left = cutPortion * diameter;
+
+    return SizedBox(
+      height: 36,
+      child: Stack(
+        children: [
+          ...List.generate(
+            4,
+            (index) {
+              if (index == 3) {
+                return Positioned(
+                  left: left * index,
+                  child: CircleAvatar(
+                    radius: avatarRadius,
+                    backgroundColor: Colors.black,
+                    child: Text(
+                      '+${avatars.length - 3}',
+                      style: const TextStyle(color: Colors.white, fontSize: 13),
+                    ),
+                  ),
+                );
+              }
+              return Positioned(
+                left: left * index,
+                child: CircleAvatar(
+                  backgroundImage: AssetImage(avatars[index]),
+                  radius: avatarRadius,
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
