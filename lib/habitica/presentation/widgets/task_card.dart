b894abc8a53928d772372ed9732d5f3d9960ac14
Quy Do Ngoc <quydn720@gmail.com>
2023-02-14 10:15:20 +0700

    add avatar mock assets

--- lib/habitica/presentation/widgets/task_card.dart
+++ lib/habitica/presentation/widgets/task_card.dart
@@ -176,3 +176,10 @@ class AvatarStacked extends StatelessWidget {
     );
   }
 }
+
+const mockAvatars = <String>[
+  'assets/images/habitica/avatar-1.jpeg',
+  'assets/images/habitica/avatar-2.jpeg',
+  'assets/images/habitica/avatar-3.jpeg',
+  'assets/images/habitica/avatar-4.jpeg',
+];
